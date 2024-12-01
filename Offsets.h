bb910f83b441341bccb40cc56e3c8d410d8cdc4e
Jeremy1926 <129874716+Jeremy1926@users.noreply.github.com>
2024-12-02 00:37:03 +0100

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -2,7 +2,7 @@ Fortnite Offsets Patch 32.11
 
 namespace WORLD
 {
-   uintptr_t UWorld = 0x12e8ef68;
+   uintptr_t UWorld = 0x13723c18;
    uintptr_t GNames = 0x130c2a80;
 }
 
@@ -12,21 +12,21 @@ namespace Engine
     uintptr_t PlayerController = 0x30;
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerState = 0x2c8;
-    uintptr_t GameState = 0x180;
-    uintptr_t GameInstance = 0x1f8;
+    uintptr_t GameState = 0x188;
+    uintptr_t GameInstance = 0x200;
     uintptr_t PlayerArray = 0x2c0;
-    uintptr_t Levels = 0x190;
-    uintptr_t BoneArray = 0x580; 
+    uintptr_t Levels = 0x1a0;
+    uintptr_t BoneArray = 0x5a8; 
     uintptr_t BoneCache = 0x5b8;
-    uintptr_t ComponentToWorld = 0x1c0;
+    uintptr_t ComponentToWorld = 0x1e0;
     uintptr_t Mesh = 0x328;
     uintptr_t PersistentLevel = 0x40;
     uintptr_t PawnPrivate = 0x320;
     uintptr_t GlobalAnimRateScale = 0x9f0;
     uintptr_t CustomTimeDilation = 0x68;
-    uintptr_t PNameStructure = 0xad0;
-    uintptr_t SpeedScale = 0x1f64;
-    uintptr_t GravityScale = 0x1f60;
+    uintptr_t pNameStructure = 0xAF8;
+    uintptr_t SpeedScale = 0x1A24;
+    uintptr_t GravityScale = 0x1DF0;
 }
 
 namespace Game
@@ -35,25 +35,25 @@ namespace Game
     uintptr_t DisplayName = 0x40;
     uintptr_t Velocity = 0x168;
     uintptr_t DefaultFOV = 0x2bc;
-    uintptr_t CurrentWeapons = 0x9f8;
+    uintptr_t CurrentWeapon = 0xa80;
     uintptr_t bIsReloadingWeapon = 0x3b9;
     uintptr_t bIsEquippingWeapon = 0x3a0;
     uintptr_t bIsChargingWeapon = 0x3b8;
     uintptr_t WeaponData = 0x568;
-    uintptr_t AmmoCount = 0xfac;
-    uintptr_t bIsTargeting = 0x1ed4;
+    uintptr_t AmmoCount = 0x104c;
+    uintptr_t bIsTargeting = 0x1fd4;
     uintptr_t Tier = 0xa2;
     uintptr_t bIsCrouched = 0x440;
-    uintptr_t bIsDying = 0x6f0;
-    uintptr_t bIsHiddenForDeath = 0x6f0;
-    uintptr_t bIsKnockedback = 0x6f1;
-    uintptr_t bIsStaggered = 0x6f1;
-    uintptr_t bIsInvulnerable = 0x6f2;
-    uintptr_t bSpotted = 0x75A;
-    uintptr_t bIsSliding = 0x6f2;
-    uintptr_t bWeaponHolstered = 0x911;
-    uintptr_t bIsDBNO = 0x912;
+    uintptr_t bIsDying = 0x718;
+    uintptr_t bIsHiddenForDeath = 0x718;
+    uintptr_t bIsKnockedback = 0x719;
+    uintptr_t bIsStaggered = 0x719;
+    uintptr_t bIsInvulnerable = 0x71a;
+    uintptr_t bSpotted = 0x71a;
+    uintptr_t bIsSliding = 0x71b;
+    uintptr_t bWeaponHolstered = 0x961;
+    uintptr_t bIsDBNO = 0x962;
     uintptr_t RevivedFromDBNOTime = 0x4d08;
-    uintptr_t bIsJumping = 0x9f0;
+    uintptr_t bIsJumping = 0xa78;
     uintptr_t bIsABot = 0x2b2;
 }
